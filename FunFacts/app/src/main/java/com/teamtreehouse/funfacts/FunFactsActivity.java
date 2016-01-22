d45a28a0a8af100a3781621287567f998084d72b
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-01-23 00:47:40 +0200

    Get started with Fun Facts

---
+++ FunFacts/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = "Ostriches can run faster than horser.";
+                mFactTextView.setText(fact);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
