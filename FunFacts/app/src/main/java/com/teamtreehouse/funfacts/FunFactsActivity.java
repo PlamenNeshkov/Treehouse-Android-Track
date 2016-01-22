da2e9eb55774449b7739fc7d62ee3e200a80d6ae
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-01-23 00:58:06 +0200

    Choose a random fact

--- FunFacts/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -24,12 +24,22 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fact = "";
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-                fact = randomNumber + "";
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
 
-                mFactTextView.setText(fact);
+                int randomNumber = new Random().nextInt(facts.length);
+
+                mFactTextView.setText(facts[randomNumber]);
             }
         };
         mShowFactButton.setOnClickListener(listener);
