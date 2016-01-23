07f5e5b7682cfb7c419283ae63b57af0b54f0e78
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-01-23 02:04:16 +0200

    Change background color dynamically

--- FunFacts/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class FunFactsActivity extends AppCompatActivity {
+public class FunFactsActivity egixtends AppCompatActivity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +22,15 @@ public class FunFactsActivity extends AppCompatActivity {
 
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mFactTextView.setText(mFactBook.getFact());
+                int color = mColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
             }
         };
         mShowFactButton.setOnClickListener(listener);
